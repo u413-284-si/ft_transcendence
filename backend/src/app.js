e3d4a49e68c6fe82b29edba82d0f8a974ab7ad54
Sirui <sirui.qiu01@gmail.com>
2025-03-05 16:18:52 +0100

    fix: update fastify to latest, repair logging with pino-pretty

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,20 @@ import env from "./config/env.js";
 import logger from "./config/logger.js";
 import routes from "./routes/routes.js";
 import dbConnector from "./config/db.js";
+import fastifyFormbody from "@fastify/formbody";
 
 const fastify = Fastify({
-  logger: logger,
+  logger: {
+		level: "info",
+		transport: {
+			target: "pino-pretty",
+		}
+	}
 });
 
+
 fastify.register(dbConnector);
+await fastify.register(fastifyFormbody);
 await fastify.register(routes);
 
 fastify.listen({ port: env.port }, (err, address) => {
