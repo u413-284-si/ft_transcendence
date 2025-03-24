a379ae23526299267f5234704e8efaf8a46778c1
Gabriel Wolf <gwolf@student.42vienna.com>
2025-03-24 20:53:40 +0100

    feat: cleanup user controller

--- app/backend/src/controllers/user.controller.js
+++ app/backend/src/controllers/user.controller.js
@@ -1,32 +1,4 @@
-import Ajv from "ajv";
 import prisma from "../utils/prisma.js";
-
-const ajv = new Ajv();
-
-/**
- * The pattern for the username is:
- * - At least 1 character
- * - At most 100 characters
- * - No whitespaces
- * - Allows lowercase and uppercase letters
- * - Allows numbers
- * - Allows special characters: !@#$%^&*(),.?":{}|<>
- */
-const usernameSchema = {
-	type: "object",
-	properties: {
-		username: {
-			type: "string",
-			minLength: 1,
-			maxLength: 100,
-			pattern: "^(?!.*\\s)(?=.*[a-zA-Z0-9!@#$%^&*(),.?\":{}|<>]).+$"
-		}
-	},
-	required: ["username"],
-	additionalProperties: false,
-};
-
-const validateUsername = ajv.compile(usernameSchema);
 
 /**
  * Controller function to add a new user
@@ -37,14 +9,6 @@ const validateUsername = ajv.compile(usernameSchema);
 export async function addUser(request, reply) {
 	const { username } = request.body;
 
-	const valid = validateUsername({ username });
-	if (!valid) {
-		return reply.status(400).send({
-			error: "Invalid input",
-			details: validateUsername.errors,
-		});
-	}
-
 	try {
 		const user = await prisma.user.create({
 			data: {
@@ -53,12 +17,15 @@ export async function addUser(request, reply) {
 				authentication: { create: {} },
 				stats: { create: {} },
 				accountStatus: { create: {} }
+			},
+			select: {
+				id: true,
+				username: true,
+				email: true,
 			}
 		})
-		// const insertStatement = request.server.db.prepare("INSERT INTO users (username) VALUES (?)");
-		// const info = insertStatement.run(username);
 
-		reply.code(201).send({ success: true, message: "User registered", user });
+		return reply.code(201).send(user);
 	} catch (err) {
 		request.log.error(err);
 		reply.code(500).send({ success: false, error: "Failed to add user" });
@@ -81,13 +48,13 @@ export async function getUser(request, reply) {
 			}
 		})
 		if (!user) {
-			return reply.code(404).send({ exists: !!user, error: "User not found" });
+			return reply.code(404).send({error: "User not found" });
 		}
 
-		reply.code(200).send({ exists: !!user, user });
+		reply.code(200).send(user);
 	} catch (err) {
 		request.log.error(err);
-		reply.code(500).send({ exists: !!user, error: "Failed to retrieve user" });
+		reply.code(500).send({error: "Failed to retrieve user"});
 	}
 }
 
@@ -125,16 +92,6 @@ export async function getUsers(request, reply) {
 export async function editUser(request, reply) {
 	const { username } = request.body;
 	const id = parseInt(request.params.id, 10);
-	if (isNaN(id))
-		return reply.status(400).send({ error: "Invalid user id" });
-
-	const valid = validateUsername({ username });
-	if (!valid) {
-		return reply.status(400).send({
-			error: "Invalid input",
-			details: validateUsername.errors,
-		});
-	}
 
 	try {
 		const user = await prisma.user.update({
@@ -143,6 +100,11 @@ export async function editUser(request, reply) {
 			},
 			data: {
 				username: username
+			},
+			select: {
+				id: true,
+				username: true,
+				email: true,
 			}
 		})
 
@@ -164,8 +126,6 @@ export async function editUser(request, reply) {
  */
 export async function deleteUser(request, reply) {
 	const id = parseInt(request.params.id, 10);
-	if (isNaN(id))
-		return reply.status(400).send({ error: "Invalid user id" });
 
 	try {
 		const user = await prisma.user.delete({
