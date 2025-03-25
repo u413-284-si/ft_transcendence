ecae9821a861d46e8352e3bc3a77dc840aea8cc6
Gabriel Wolf <gwolf@student.42vienna.com>
2025-03-25 09:05:23 +0100

    feat: implement get routes for match

--- app/backend/src/controllers/match.controller.js
+++ app/backend/src/controllers/match.controller.js
@@ -30,3 +30,71 @@ export async function createMatch(request, reply) {
 		reply.code(500).send({ error: "Failed to add match" });
 	}
 }
+
+export async function getMatches(request, reply) {
+	try {
+		const matches = await prisma.match.findMany({
+			select: { id: true }
+		}
+		);
+
+		// Respond with the list of matches
+		reply.code(200).send(matches);
+	} catch (err) {
+		// Log the error and respond with a 500 status code
+		request.log.error(err);
+		reply.code(500).send({ error: "Failed to retrieve matches" });
+	}
+}
+
+export async function getMatch(request, reply) {
+	const id = parseInt(request.params.id, 10);
+
+	try {
+		const match = await prisma.match.findUnique({
+			where: {
+				id
+			}
+		})
+		if (!match) {
+			return reply.code(404).send({ error: "Match not found" });
+		}
+
+		reply.code(200).send(match);
+	} catch (err) {
+		request.log.error(err);
+		reply.code(500).send({ error: "Failed to retrieve Match" });
+	}
+}
+
+export async function getMatchesByUserId(request, reply) {
+	const playerId = parseInt(request.params.id, 10);
+
+	try {
+		const matches = await prisma.match.findMany({
+			where: {
+				playerId
+			},
+			select: {
+				playerNickname: true,
+				opponentNickname: true,
+				playerScore: true,
+				opponentScore: true,
+				date: true,
+				player: {
+					select: {
+						username: true
+					}
+				}
+			}
+		})
+		if (!matches) {
+			return reply.code(404).send({ error: "No matches found for this user" });
+		}
+
+		reply.code(200).send(matches);
+	} catch (err) {
+		request.log.error(err);
+		reply.code(500).send({ error: "Failed to retrieve matches" });
+	}
+}
--- app/backend/src/routes/match.routes.js
+++ app/backend/src/routes/match.routes.js
@@ -1,13 +1,25 @@
-import { createMatch } from "../controllers/match.controller.js";
+import { createMatch, getMatches, getMatch, getMatchesByUserId } from "../controllers/match.controller.js";
 
 export default async function matchRoutes(fastify) {
 
-	fastify.post("/",
-		{
-			schema: {
-				body: { $ref: "createMatchSchema" }
-			}
-		},
-		createMatch);
+	fastify.post("/", optionsCreateMatch, createMatch);
 
+	fastify.get("/", getMatches);
+
+	fastify.get("/:id", optionsGetMatch, getMatch);
+
+	fastify.get("/user/:id", optionsGetMatch, getMatchesByUserId);
+
+}
+
+const optionsCreateMatch = {
+	schema: {
+		body: { $ref: "createMatchSchema" }
+	}
+}
+
+const optionsGetMatch = {
+	schema: {
+		params: { $ref: "idSchema" },
+	}
 }
