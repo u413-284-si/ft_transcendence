f0b06fc6a31388c7c4591d3ce0939a1816430aaf
Gabriel Wolf <gwolf@student.42vienna.com>
2025-03-25 18:22:50 +0100

    feat: implement user stats get routes

--- app/backend/src/controllers/userStats.controller.js
+++ app/backend/src/controllers/userStats.controller.js
@@ -1,5 +1,36 @@
 import prisma from "../prisma/prismaClient.js";
 
+export async function getUserStats(request, reply) {
+	try {
+		const userStats = await prisma.userStats.findMany();
+		if (!userStats) {
+			return reply.code(404).send({ error: "No user stats found" });
+		}
+		reply.code(200).send(userStats);
+	} catch (err) {
+		request.log.error(err);
+		reply.code(500).send({ error: "Failed to retrieve user stats" });
+	}
+}
+
+export async function getUserStat(request, reply) {
+	const userId = parseInt(request.params.id, 10);
+	try {
+		const userStat = await prisma.userStats.findUnique({
+			where: {
+				userId
+			}
+		})
+		if (!userStat) {
+			return reply.code(404).send({ error: "User stat not found" });
+		}
+		reply.code(200).send(userStat);
+	} catch (err) {
+		request.log.error(err);
+		reply.code(500).send({ error: "Failed to retrieve user" });
+	}
+}
+
 export async function updateUserStats(request, reply) {
 	const userId = parseInt(request.params.id, 10);
 	const { matchWon } = request.body;
@@ -13,7 +44,6 @@ export async function updateUserStats(request, reply) {
 				matchesLost: { increment: matchWon ? 0 : 1 }
 			}
 		})
-
 		reply.code(200).send({ message: "Stats updated", ...stats });
 	} catch (err) {
 		request.log.error(err);
@@ -22,5 +52,4 @@ export async function updateUserStats(request, reply) {
 		else
 			reply.code(500).send({ error: "Failed to retrieve Match" });
 	}
-
 }
--- app/backend/src/routes/userStats.routes.js
+++ app/backend/src/routes/userStats.routes.js
@@ -1,9 +1,17 @@
-import { updateUserStats } from "../controllers/userStats.controller.js";
+import { getUserStats, getUserStat, updateUserStats } from "../controllers/userStats.controller.js";
 
 export default async function userStatsRoutes(fastify) {
 
+	fastify.get("/", getUserStats);
+	fastify.get("/:id", optionsGetUserStats, getUserStat);
 	fastify.put("/:id", optionsUpdateUserStats, updateUserStats);
 
+}
+
+const optionsGetUserStats = {
+	schema: {
+		params: { $ref: "idSchema" },
+	}
 }
 
 const optionsUpdateUserStats = {
