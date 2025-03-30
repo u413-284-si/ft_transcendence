6810d59ff9c18d824ba0d3bd420a0f9b6baab8d3
sirui <sirui.qiu01@gmail.com>
2025-03-30 12:34:11 +0200

    refactor: move GameState creation into initGameState()

--- app/frontend/src/game.ts
+++ app/frontend/src/game.ts
@@ -1,6 +1,6 @@
 import { updatePaddlePositions, setupInputListeners } from "./input.js";
 import { draw } from "./draw.js";
-import { IGameState } from "./IGameState.js";
+import { IGameState } from "./types/IGameState.js";
 
 export function renderGame() {
 	const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
@@ -14,26 +14,29 @@ export function renderGame() {
 	if (!nickname1 || !nickname2)
 		return alert("Please enter a nickname for both players.");
 
-	const gameState: IGameState = {
-		player1: nickname1,
-		player2: nickname2,
-		player1Score: 0,
-		player2Score: 0,
-		winningScore: 3,
-		ballX: canvas.width / 2,
-		ballY: canvas.height / 2,
-		ballSpeedX: 7,
-		ballSpeedY: 7,
-		paddle1Y: canvas.height / 2 - 40,
-		paddle2Y: canvas.height / 2 - 40,
-		paddleHeight: 80,
-		paddleWidth: 10,
-		paddleSpeed: 6,
-		gameStarted: false,
-		gameOver: false
-	};
-
+	const gameState = initGameState(canvas, nickname1, nickname2);
 	startGame(canvas, ctx, gameState);
+}
+
+function initGameState(canvas: HTMLCanvasElement, nickname1: string, nickname2: string): IGameState {
+  return {
+      player1: nickname1,
+      player2: nickname2,
+      player1Score: 0,
+      player2Score: 0,
+      winningScore: 3,
+      ballX: canvas.width / 2,
+      ballY: canvas.height / 2,
+      ballSpeedX: 7,
+      ballSpeedY: 7,
+      paddle1Y: canvas.height / 2 - 40,
+      paddle2Y: canvas.height / 2 - 40,
+      paddleHeight: 80,
+      paddleWidth: 10,
+      paddleSpeed: 6,
+      gameStarted: false,
+      gameOver: false
+  };
 }
 
 function startGame(canvas: HTMLCanvasElement,  ctx: CanvasRenderingContext2D, gameState: IGameState) {
