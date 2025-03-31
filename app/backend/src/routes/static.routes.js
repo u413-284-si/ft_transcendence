02ba106aa446f511a73537a7159165512b260c02
Gabriel Wolf <gwolf@student.42vienna.com>
2025-03-31 16:22:18 +0200

    fix: adapt error message

--- app/backend/src/routes/static.routes.js
+++ app/backend/src/routes/static.routes.js
@@ -6,11 +6,8 @@ export default async function staticRoutes(fastify) {
 	fastify.setNotFoundHandler((req, reply) => {
 		if (req.raw.url && req.raw.url.startsWith("/api")) {
 			return reply.status(404).send({
-				sucess: false,
-				error: {
-					kind: "user_input",
-					message: "Not found"
-				}
+				message: `Fail: ${req.raw.url}`,
+				cause: "Route does not exist"
 			});
 		}
 		return reply.status(200).sendFile("index.html");
