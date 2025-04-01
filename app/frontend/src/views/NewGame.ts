5eb94535e6220c587810aa527251f55505ae27e3
Sirui <sirui.qiu01@gmail.com>
2025-04-01 23:17:05 +0200

    format: indent html

--- app/frontend/src/views/NewGame.ts
+++ app/frontend/src/views/NewGame.ts
@@ -9,15 +9,15 @@ export default class extends AbstractView {
 
   async createHTML() {
     return `
-			<form id="register-form">
-				<label for="nickname1">Player 1 Nickname:</label>
-				<input type="text" id="nickname1" placeholder="Enter nickname"><br><br>
-				<label for="nickname2">Player 2 Nickname:</label>
-				<input type="text" id="nickname2" placeholder="Enter nickname"><br><br>
-				<button id="start-button">Start Game</button>
-			</form>
-			<canvas id="gameCanvas" width="800" height="400" class="border-4 border-white"></canvas>
-			`;
+            <form id="register-form">
+                <label for="nickname1">Player 1 Nickname:</label>
+                <input type="text" id="nickname1" placeholder="Enter nickname"><br><br>
+                <label for="nickname2">Player 2 Nickname:</label>
+                <input type="text" id="nickname2" placeholder="Enter nickname"><br><br>
+                <button id="start-button">Start Game</button>
+            </form>
+            <canvas id="gameCanvas" width="800" height="400" class="border-4 border-white"></canvas>
+            `;
   }
 
   async addListeners() {
