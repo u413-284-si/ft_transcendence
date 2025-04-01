b1c17841dfa2841e0ca0d6d173081ab0ad96954e
Sirui <sirui.qiu01@gmail.com>
2025-04-01 22:26:54 +0200

    style: add styling to NewTournament view

--- app/frontend/src/views/NewTournament.ts
+++ app/frontend/src/views/NewTournament.ts
@@ -9,24 +9,37 @@ export default class extends AbstractView {
 
   async createHTML() {
     return `
-    <h1>New Tournament</h1>
-            <p>Enter the tournament name and select the number of players:</p>
-            <form id="tournament-form">
-                <label>
-                    Tournament Name:
-                    <input type="text" name="tournamentName" required>
-                </label><br><br>
-                <label>
-                    <input type="radio" name="players" value="4" required> 4 Players
-                </label><br>
-                <label>
-                    <input type="radio" name="players" value="8"> 8 Players
-                </label><br>
-                <label>
-                    <input type="radio" name="players" value="16"> 16 Players
-                </label><br>
-                <button type="submit">Start Tournament</button>
-            </form>
+        <h1 style="
+            margin-bottom: 40px; 
+            font-size: 2.5em; 
+            color: #FF00AA; 
+            text-align: center; 
+            text-shadow: 4px 4px 8px rgba(0, 0, 0, 0.4);"
+        >
+            New Tournament
+        </h1>
+        <p style="margin-bottom: 20px;">Enter the tournament name and select the number of players:</p>
+        <form id="tournament-form">
+            <label style="font-size: 1.2em; font-weight: bold; display: block; margin-bottom: 10px;">
+                Tournament Name:
+                <input 
+                    type="text"
+                    name="tournamentName"
+                    required
+                    style="width: 30%; padding: 10px; font-size: 1em; border: 2px solid #007BFF; border-radius: 5px; margin-top: 5px;"
+                >
+            </label><br><br>
+            <label>
+                <input type="radio" name="players" value="4" required> 4 Players
+            </label><br>
+            <label>
+                <input type="radio" name="players" value="8"> 8 Players
+            </label><br>
+            <label>
+                <input type="radio" name="players" value="16"> 16 Players
+            </label><br>
+            <button type="submit">Start Tournament</button>
+        </form>
         `;
   }
 
