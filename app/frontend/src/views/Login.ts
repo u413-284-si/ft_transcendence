78b7099f91fbed4387cf2e1ee3605c0364d1d165
QCHR1581 <chris1581.at@gmail.com>
2025-04-02 22:46:27 +0200

    feat: add login view file

---
+++ app/frontend/src/views/Login.ts
@@ -0,0 +1,30 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+	constructor() {
+		super();
+		this.setTitle("Login");
+	}
+
+	async createHTML() {
+		return `
+				<form id="login-form"  class="flex flex-col justify-center items-center h-screen gap-4">
+					<div>
+						<label for="usernameOrEmail">Username or Email:</label><br>
+						<input id="usernameOrEmail" name="usernameOrEmail" type="text" class="border-2 border-white rounded-sm">
+					</div>
+					<div>
+						<label for="password">Password:</label><br>
+						<input id="password" name="password" type="text" class="border-2 border-white rounded-sm">
+					</div>
+					<div>
+						<button id="login-button" class="border-2 border-white rounded-sm p-2">Login</button>
+					</div>
+				</form>
+			`;
+	}
+
+	async render() {
+		await this.updateHTML();
+	}
+}
