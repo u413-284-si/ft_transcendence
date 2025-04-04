9e0366ddb070f37072d1970465483b5497f63a88
Gabriel Wolf <gwolf@student.42vienna.com>
2025-04-04 10:21:46 +0000

    fix: remove escape char from regex

--- app/backend/src/schema/common.schema.js
+++ app/backend/src/schema/common.schema.js
@@ -5,9 +5,9 @@ export const commonDefinitionsSchema = {
       type: "string",
       minLength: 3,
       maxLength: 20,
-      pattern: "^[a-zA-Z0-9!?\-_@$.]{3,20}$",
+      pattern: "^[a-zA-Z0-9-!?_@$.]{3,20}$",
       description:
-        "A unique username with 3-20 alphanumeric or the following special characters inside brackets: [!?-_@$.]"
+        "A unique username with 3-20 alphanumeric or the following special characters inside brackets: [-!?_@$.]"
     },
     email: {
       type: "string",
