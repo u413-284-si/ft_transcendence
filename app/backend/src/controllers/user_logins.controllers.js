e89789f5080c80e78b9b1747dd72eec8189930b1
QCHR1581 <chris1581.at@gmail.com>
2025-04-05 10:10:48 +0200

    fix: add var action

--- app/backend/src/controllers/user_logins.controllers.js
+++ app/backend/src/controllers/user_logins.controllers.js
@@ -1,14 +1,20 @@
 import { loginUser } from "../services/user_logins.services.js";
 import { handlePrismaError } from "../utils/error.js";
-import { createResponseMessage } from "../utils/response.js"
+import { createResponseMessage } from "../utils/response.js";
 
 export async function loginUserHandler(request, reply) {
-	try {
-		const { usernameOrEmail, password } = request.body;
-		const user = await loginUser(usernameOrEmail, password);
-		return reply.code(200).send({ message: createResponseMessage(action, true), user });
-	} catch (err) {
-		request.log.error({ err, body: request.body }, `loginUserHandler: ${createResponseMessage(action, false)}`);
-		return handlePrismaError(reply, action, err);
-	}
+  const action = "Login user";
+  try {
+    const { usernameOrEmail, password } = request.body;
+    const user = await loginUser(usernameOrEmail, password);
+    return reply
+      .code(200)
+      .send({ message: createResponseMessage(action, true), user });
+  } catch (err) {
+    request.log.error(
+      { err, body: request.body },
+      `loginUserHandler: ${createResponseMessage(action, false)}`
+    );
+    return handlePrismaError(reply, action, err);
+  }
 }
