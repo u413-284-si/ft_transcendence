52582512e160ec2681262578bc85380b655960dd
sirui <sirui.qiu01@gmail.com>
2025-04-06 20:27:10 +0000

    fix: invoke callGameView() explicitly and prevent default

--- app/frontend/src/views/MatchAnnouncement.ts
+++ app/frontend/src/views/MatchAnnouncement.ts
@@ -1,6 +1,6 @@
 import { Tournament } from "../Tournament.js";
 import AbstractView from "./AbstractView.js";
-import { GameView, GameType } from "./Game.js";
+import { GameView, GameType } from "./GameView.js";
 
 export default class extends AbstractView {
   private player1: string | null = null;
@@ -9,7 +9,7 @@ export default class extends AbstractView {
   constructor(private tournament: Tournament) {
     super();
     this.setTitle("Match Announcement");
-    const match = this.tournament.getNextBracket();
+    const match = this.tournament.getNextMatch();
     if (!match) {
       // render WinView;
       return;
@@ -33,7 +33,7 @@ export default class extends AbstractView {
       </p>
       <div style="text-align: center;">
         <form id="match-form">
-          <button type="submit" id="start-match" style="
+          <button type="submit" style="
             margin-top: 20px; 
             padding: 10px 20px; 
             font-size: 1em; 
@@ -52,8 +52,8 @@ export default class extends AbstractView {
 
   async addListeners() {
     document
-      .getElementById("start-match")
-      ?.addEventListener("submit", () => this.callGameView);
+      .getElementById("match-form")
+      ?.addEventListener("submit", (event) => this.callGameView(event));
   }
 
   async render() {
@@ -61,10 +61,11 @@ export default class extends AbstractView {
     this.addListeners();
   }
 
-  private callGameView() {
-    // console.log(
-    //   `Match ${this.tournament.getMatchNumber()} started: ${this.player1} vs ${this.player2}`
-    // );
+  private callGameView(event: Event) {
+    event.preventDefault();
+    console.log(
+      `Match ${this.tournament.getMatchNumber()} started: ${this.player1} vs ${this.player2}`
+    );
 
     if (!this.player1 || !this.player2) {
       console.error("Player names are not set.");
