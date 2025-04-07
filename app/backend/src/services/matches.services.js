0d811d0f4506e62e475541fd0642e44d5e869677
Gabriel Wolf <gwolf@student.42vienna.com>
2025-04-07 08:52:59 +0000

    fix: update tournament status when match saved

--- app/backend/src/services/matches.services.js
+++ app/backend/src/services/matches.services.js
@@ -26,6 +26,18 @@ export async function createMatch(
       opponentScore: true
     }
   });
+  if (tournamentId) {
+    const tournament = await prisma.tournament.findUniqueOrThrow({
+      where: { id: tournamentId }
+    });
+
+    if (tournament && tournament.status === "CREATED") {
+      await prisma.tournament.update({
+        where: { id: tournament.id },
+        data: { status: "IN_PROGRESS" }
+      });
+    }
+  }
   return match;
 }
 
