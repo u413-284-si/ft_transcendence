e038ecc8aad8f74bdbd1dd1823a8f488aa737132
QCHR1581 <chris1581.at@gmail.com>
2025-04-07 07:03:32 +0000

    fix: remove @ from allowed characters for username

--- app/backend/src/schema/common.schema.js
+++ app/backend/src/schema/common.schema.js
@@ -5,7 +5,7 @@ export const commonDefinitionsSchema = {
       type: "string",
       minLength: 3,
       maxLength: 20,
-      pattern: "^[a-zA-Z0-9-!?_@$.]{3,20}$",
+      pattern: "^[a-zA-Z0-9-!?_$.]{3,20}$",
       description:
         "A unique username with 3-20 alphanumeric or the following special characters inside brackets: [-!?_@$.]"
     },
