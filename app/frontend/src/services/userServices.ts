4311cb29a7bd394ed668fc5dd228dc99a1a5679a
Gabriel Wolf <gwolf@student.42vienna.com>
2025-04-16 08:00:37 +0000

    fix: add back localhost to services

--- app/frontend/src/services/userServices.ts
+++ app/frontend/src/services/userServices.ts
@@ -2,10 +2,13 @@ import { apiFetch } from "./api.js";
 import { Match } from "../types/IMatch.js";
 
 export async function getUserMatches(): Promise<Match[]> {
-  const apiResponse = await apiFetch<Match[]>("/api/users/matches/", {
-    method: "GET",
-    credentials: "same-origin"
-  });
+  const apiResponse = await apiFetch<Match[]>(
+    "http://localhost:4000/api/users/matches/",
+    {
+      method: "GET",
+      credentials: "same-origin"
+    }
+  );
 
   console.log(apiResponse);
   return apiResponse.data;
