6e38f1ca0dd5bde8de65d52f76126835393b2a96
sirui <sirui.qiu01@gmail.com>
2025-04-28 19:21:38 +0000

    refactor: validate login

--- app/frontend/src/validate.ts
+++ app/frontend/src/validate.ts
@@ -1,17 +1,29 @@
-export function validateNicknames(nicknames: string[]): boolean {
-  const usernameRegex = /^[a-zA-Z0-9-!?_$.]{3,20}$/;
+export function validateName(name: string): boolean {
+  const nameRegex = /^[a-zA-Z0-9-!?_$.]{3,20}$/;
 
+  if (name === "") {
+    alert("Please enter a name.");
+    return false;
+  }
+
+  if (!nameRegex.test(name)) {
+    alert(
+      "Names must be 3-20 characters long and can only contain letters, " +
+        "numbers, or the following special characters inside brackets: [-!?_$.]."
+    );
+    return false;
+  }
+  return true;
+}
+
+export function validateNicknames(nicknames: string[]): boolean {
   if (nicknames.some((nickname) => nickname === "")) {
     alert("Please enter a nickname for all players.");
     return false;
   }
 
-  if (!nicknames.every((nickname) => usernameRegex.test(nickname))) {
-    alert(
-      "Nicknames must be 3-20 characters long and can only contain letters, " +
-        "numbers, or the following special characters inside brackets: [-!?_$.]."
-    );
-    return false;
+  for (const nickname of nicknames) {
+    if (!validateName(nickname)) return false;
   }
 
   if (hasDuplicates(nicknames)) {
@@ -49,4 +61,47 @@ export function validatePlayers(players: HTMLInputElement): boolean {
     return false;
   }
   return true;
+}
+
+export function validatePassword(password: string): boolean {
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z@$!%*?&]{14,30}$/;
+
+  if (password === "") {
+    alert("Please enter a password.");
+    return false;
+  }
+
+  if (!passwordRegex.test(password)) {
+    alert(
+      "Password must be 14-30 characters long and must contain at least one " +
+        "number, one uppercase and one lowercase letter and one of the " +
+        "following special characters inside brackets: [-!?_$.]."
+    );
+    return false;
+  }
+  return true;
+}
+
+export function validateEmail(email: string): boolean {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  if (email === "") {
+    alert("Please enter an email address.");
+    return false;
+  }
+
+  if (!emailRegex.test(email)) {
+    alert(
+      "Please enter a valid email address in the format example@domain.com."
+    );
+    return false;
+  }
+  return true;
+}
+
+export function validateUsernameOrEmail(usernameOrEmail: string): boolean {
+  if (validateName(usernameOrEmail) || validateEmail(usernameOrEmail))
+    return true;
+  return false;
 }
--- app/frontend/src/views/Login.ts
+++ app/frontend/src/views/Login.ts
@@ -1,5 +1,10 @@
 import AbstractView from "./AbstractView.js";
-import { loginUser } from "../login.js";
+import { navigateTo } from "../main.js";
+import { ApiError } from "../services/api.js";
+import { userLogin } from "../services/authServices.js";
+// FIXME: activate when password policy is applied
+//import { validatePassword, validateUsernameOrEmail } from "../validate.js";
+import { validateUsernameOrEmail } from "../validate.js";
 
 export default class extends AbstractView {
   constructor() {
@@ -27,11 +32,39 @@ export default class extends AbstractView {
 
   async addListeners() {
     const loginForm = document.getElementById("login-form") as HTMLFormElement;
-    loginForm.addEventListener("submit", (event) => loginUser(event));
+    loginForm.addEventListener("submit", (event) =>
+      validateAndLoginUser(event)
+    );
   }
 
   async render() {
     await this.updateHTML();
     await this.addListeners();
   }
+}
+
+export async function validateAndLoginUser(event: Event): Promise<void> {
+  event.preventDefault();
+  const loginForm: HTMLFormElement = document.getElementById(
+    "login-form"
+  ) as HTMLFormElement;
+  const usernameOrEmail: string = loginForm.usernameOrEmail.value;
+  const password: string = loginForm.password.value;
+
+  if (!validateUsernameOrEmail(usernameOrEmail)) return;
+  // FIXME: activate when password policy is applied
+  // if (!validatePassword(password)) return;
+
+  try {
+    await userLogin(usernameOrEmail, password);
+  } catch (error) {
+    if (error instanceof ApiError) {
+      if (error.status === 401) {
+        alert("Invalid username or password");
+      }
+    }
+    console.error(error);
+  }
+
+  navigateTo("/home");
 }
