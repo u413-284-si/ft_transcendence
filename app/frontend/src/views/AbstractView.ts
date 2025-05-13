ce3e730c951365cc0d3ae83c55c997282cca8aa1
Gabriel Wolf <gwolf@student.42vienna.com>
2025-05-13 14:16:06 +0000

    fix: revert nav text to blue-300

--- app/frontend/src/views/AbstractView.ts
+++ app/frontend/src/views/AbstractView.ts
@@ -8,11 +8,11 @@ export default abstract class {
   async createNavbar() {
     return `<nav class="bg-blue-800 text-white py-4 shadow-lg">
 					<div class="container mx-auto flex justify-center space-x-8">
-						<a href="/home" class="hover:text-indigo-600" data-link>Home</a>
-						<a href="/newGame" class="hover:text-indigo-600" data-link>New Game</a>
-						<a href="/newTournament" class="hover:text-indigo-600" data-link>New Tournament</a>
-						<a href="/stats" class="hover:text-indigo-600" data-link>Stats</a>
-						<a href="/settings" class="hover:text-indigo-600" data-link>Settings</a>
+						<a href="/home" class="text-lg hover:text-blue-300" data-link>Home</a>
+						<a href="/newGame" class="text-lg hover:text-blue-300" data-link>New Game</a>
+						<a href="/newTournament" class="text-lg hover:text-blue-300" data-link>New Tournament</a>
+						<a href="/stats" class="text-lg hover:text-blue-300" data-link>Stats</a>
+						<a href="/settings" class="text-lg hover:text-blue-300" data-link>Settings</a>
 					</div>
 			</nav>`;
   }
