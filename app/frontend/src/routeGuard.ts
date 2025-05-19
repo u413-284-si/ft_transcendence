6ba60fbfceb91a60d468116b35ecd3cb0dbf49d0
Gabriel Wolf <gwolf@student.42vienna.com>
2025-05-19 18:01:35 +0000

    fix: add comment

--- app/frontend/src/routeGuard.ts
+++ app/frontend/src/routeGuard.ts
@@ -5,6 +5,7 @@ export const authGuard: RouteGuard = () => {
   return auth.isAuthenticated() ? true : "/login";
 };
 
+// If you are logged in and try to go to guarded page, you get redirected to /home.
 export const guestOnlyGuard: RouteGuard = () => {
   return !auth.isAuthenticated() ? true : "/home";
 };
