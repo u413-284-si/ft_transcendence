e534b1b575b309ff37b9dee31ef69c773545ba0d
Sirui <sirui.qiu01@gmail.com>
2025-05-20 19:12:47 +0000

    refactor: remove prisma middleware

--- app/backend/src/prisma/prismaClient.js
+++ app/backend/src/prisma/prismaClient.js
@@ -1,10 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-// import { updateWinRateMiddleware } from "./prismaMiddleware.js";
 
 let prisma = new PrismaClient({
   log: ["query", "info", "warn", "error"]
 });
-// FIXME: remove?
-// prisma.$use(updateWinRateMiddleware);
 
 export default prisma;
