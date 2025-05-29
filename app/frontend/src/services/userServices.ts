aa4eda0f9f8d2c6a2ebbc36c44d0f82dd4e5629e
QCHR1581 <chris1581.at@gmail.com>
2025-05-29 14:36:12 +0000

    feat: implement functionality for registering an user

--- app/frontend/src/services/userServices.ts
+++ app/frontend/src/services/userServices.ts
@@ -1,5 +1,6 @@
 import { apiFetch } from "./api.js";
 import { Match } from "../types/IMatch.js";
+import { User } from "../types/IUserCreation.js";
 
 export async function getUserMatches(): Promise<Match[]> {
   const apiResponse = await apiFetch<Match[]>("/api/users/matches/", {
@@ -7,6 +8,19 @@ export async function getUserMatches(): Promise<Match[]> {
     credentials: "same-origin"
   });
 
+  console.log(apiResponse);
+  return apiResponse.data;
+}
+
+export async function registerUser(
+  email: string,
+  username: string,
+  password: string
+) {
+  const apiResponse = await apiFetch<User>("api/users/", {
+    method: "POST",
+    body: JSON.stringify({ email, username, password })
+  });
   console.log(apiResponse);
   return apiResponse.data;
 }
