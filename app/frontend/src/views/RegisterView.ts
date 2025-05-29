ac79a7949d7f53ac6f2cff600bf4950d26971b56
QCHR1581 <chris1581.at@gmail.com>
2025-05-29 17:28:03 +0000

    feat: add eye toggle for passwords

--- app/frontend/src/views/RegisterView.ts
+++ app/frontend/src/views/RegisterView.ts
@@ -52,12 +52,57 @@ export default class LoginView extends AbstractView {
         </div>
         <div class="w-[300px]">
           <label for="password">Password:</label><br />
-          <input
-            id="password"
-            name="password"
-            type="password"
-            class="w-full border border-gray-300 rounded px-2 py-1 transition-all duration-300"
-          />
+          <div class="relative flex items-center">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              class="w-full border border-gray-300 rounded px-2 py-1 transition-all duration-300"
+            />
+            <button
+              type="button"
+              id="password-toggle"
+              class="absolute right-2 p-2 text-gray-300 hover:text-gray-50"
+            >
+              <span id="show-eye">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              </span>
+              <span id="hide-eye" class="hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                  />
+                </svg>
+              </span>
+            </button>
+          </div>
           <span
             id="password-error"
             class="error-message text-red-600 text-sm mt-1 hidden"
@@ -65,12 +110,57 @@ export default class LoginView extends AbstractView {
         </div>
         <div class="w-[300px]">
           <label for="confirm-password">Confirm Password:</label><br />
-          <input
-            id="confirm-password"
-            name="confirm-password"
-            type="password"
-            class="w-full border border-gray-300 rounded px-2 py-1 transition-all duration-300"
-          />
+          <div class="relative flex items-center">
+            <input
+              id="confirm-password"
+              name="confirm-password"
+              type="password"
+              class="w-full border border-gray-300 rounded px-2 py-1 transition-all duration-300"
+            />
+            <button
+              type="button"
+              id="confirm-password-toggle"
+              class="absolute right-2 p-2 text-gray-300 hover:text-gray-50"
+            >
+              <span id="confirm-show-eye">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              </span>
+              <span id="confirm-hide-eye" class="hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                  />
+                </svg>
+              </span>
+            </button>
+          </div>
           <span
             id="confirm-password-error"
             class="error-message text-red-600 text-sm mt-1 hidden"
@@ -91,6 +181,12 @@ export default class LoginView extends AbstractView {
       ?.addEventListener("submit", (event) =>
         this.validateAndRegisterUser(event)
       );
+    document
+      .getElementById("password-toggle")
+      ?.addEventListener("click", () => this.togglePasswordVisibility());
+    document
+      .getElementById("confirm-password-toggle")
+      ?.addEventListener("click", () => this.toggleConfirmPasswordVisibility());
   }
 
   async render() {
@@ -100,6 +196,46 @@ export default class LoginView extends AbstractView {
 
   getName(): string {
     return "register";
+  }
+
+  private async togglePasswordVisibility() {
+    const passwordEl = document.getElementById("password") as HTMLInputElement;
+
+    const showEyeEl = document.getElementById("show-eye") as HTMLElement;
+    const hideEyeEl = document.getElementById("hide-eye") as HTMLElement;
+
+    if (passwordEl.type === "password") {
+      passwordEl.type = "text";
+      showEyeEl.classList.add("hidden");
+      hideEyeEl.classList.remove("hidden");
+    } else {
+      passwordEl.type = "password";
+      showEyeEl.classList.remove("hidden");
+      hideEyeEl.classList.add("hidden");
+    }
+  }
+
+  private async toggleConfirmPasswordVisibility() {
+    const confirmPasswordEl = document.getElementById(
+      "confirm-password"
+    ) as HTMLInputElement;
+
+    const confirmShowEyeEl = document.getElementById(
+      "confirm-show-eye"
+    ) as HTMLElement;
+    const confirmHideEyeEl = document.getElementById(
+      "confirm-hide-eye"
+    ) as HTMLElement;
+
+    if (confirmPasswordEl.type === "password") {
+      confirmPasswordEl.type = "text";
+      confirmShowEyeEl.classList.add("hidden");
+      confirmHideEyeEl.classList.remove("hidden");
+    } else {
+      confirmPasswordEl.type = "password";
+      confirmShowEyeEl.classList.remove("hidden");
+      confirmHideEyeEl.classList.add("hidden");
+    }
   }
 
   private async validateAndRegisterUser(event: Event) {
