99965f3fcce9d54316c55542cda555a6ab9f9bd9
Gabriel Wolf <gwolf@student.42vienna.com>
2025-06-04 09:23:31 +0000

    feat: add service to search User

--- app/frontend/src/services/userServices.ts
+++ app/frontend/src/services/userServices.ts
@@ -1,11 +1,27 @@
 import { apiFetch } from "./api.js";
 import { Match } from "../types/IMatch.js";
+import { Token } from "../types/Token.js";
 
 export async function getUserMatches(): Promise<Match[]> {
   const apiResponse = await apiFetch<Match[]>("/api/users/matches/", {
     method: "GET",
     credentials: "same-origin"
   });
+
+  console.log(apiResponse);
+  return apiResponse.data;
+}
+
+export async function getUserByUsername(
+  username: string
+): Promise<Token | null> {
+  const apiResponse = await apiFetch<Token | null>(
+    `/api/users/search/?username=${encodeURIComponent(username)}`,
+    {
+      method: "GET",
+      credentials: "same-origin"
+    }
+  );
 
   console.log(apiResponse);
   return apiResponse.data;
