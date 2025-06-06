3abb111cbf327bceedb62b726ad334ab16d5d2aa
Gabriel Wolf <gwolf@student.42vienna.com>
2025-06-06 12:20:39 +0000

    feat: update to new api routes

--- app/frontend/src/services/friendsServices.ts
+++ app/frontend/src/services/friendsServices.ts
@@ -1,35 +1,17 @@
 import { apiFetch } from "./api.js";
-import { Friend } from "../types/Friend.js";
 import { FriendRequest } from "../types/FriendRequest.js";
 
-export async function getUserFriends(): Promise<Friend[]> {
-  const apiResponse = await apiFetch<Friend[]>("/api/users/friends/", {
-    method: "GET",
-    credentials: "same-origin"
-  });
-
-  console.log(apiResponse);
-  return apiResponse.data;
-}
-
-export async function deleteFriend(friendId: number): Promise<Friend> {
-  const apiResponse = await apiFetch<Friend>(
-    `/api/users/friends/${friendId}/`,
+export async function createFriendRequest(
+  receiverId: number
+): Promise<FriendRequest> {
+  const apiResponse = await apiFetch<FriendRequest>(
+    "/api/users/friend-requests/",
     {
-      method: "DELETE"
+      method: "POST",
+      body: JSON.stringify({ id: receiverId }),
+      credentials: "same-origin"
     }
   );
-
-  console.log(apiResponse);
-  return apiResponse.data;
-}
-
-export async function sendFriendRequest(receiverId: number): Promise<null> {
-  const apiResponse = await apiFetch<null>("/api/users/friend-requests/", {
-    method: "POST",
-    body: JSON.stringify({ id: receiverId }),
-    credentials: "same-origin"
-  });
 
   console.log(apiResponse);
   return apiResponse.data;
@@ -48,15 +30,14 @@ export async function getUserFriendRequests(): Promise<FriendRequest[]> {
   return apiResponse.data;
 }
 
-export async function respondToFriendRequest(
-  requestId: number,
-  accept: boolean
+export async function acceptFriendRequest(
+  requestId: number
 ): Promise<FriendRequest> {
   const apiResponse = await apiFetch<FriendRequest>(
     `/api/users/friend-requests/${requestId}/`,
     {
       method: "PATCH",
-      body: JSON.stringify({ status: accept ? "ACCEPTED" : "DECLINED" }),
+      body: JSON.stringify({ status: "ACCEPTED" }),
       credentials: "same-origin"
     }
   );
