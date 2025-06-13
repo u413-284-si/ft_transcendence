48daa9d0b3e5c421aa0b90d9f3cb95f418913be9
Gabriel Wolf <gwolf@student.42vienna.com>
2025-06-13 19:35:15 +0000

    feat: add delta time to input

--- app/frontend/src/input.ts
+++ app/frontend/src/input.ts
@@ -1,21 +1,23 @@
 import { GameState } from "./types/IGameState.js";
 
 export function updatePaddlePositions(
-  canvas: HTMLCanvasElement,
-  gameState: GameState
+  gameState: GameState,
+  deltaTime: DOMHighResTimeStamp
 ) {
+  const canvas = gameState.canvas;
+
   if (gameState.keys["w"] && gameState.paddle1Y > 0)
-    gameState.paddle1Y -= gameState.paddleSpeed;
+    gameState.paddle1Y -= gameState.paddleSpeed * deltaTime;
   if (
     gameState.keys["s"] &&
     gameState.paddle1Y < canvas.height - gameState.paddleHeight
   )
-    gameState.paddle1Y += gameState.paddleSpeed;
+    gameState.paddle1Y += gameState.paddleSpeed * deltaTime;
   if (gameState.keys["ArrowUp"] && gameState.paddle2Y > 0)
-    gameState.paddle2Y -= gameState.paddleSpeed;
+    gameState.paddle2Y -= gameState.paddleSpeed * deltaTime;
   if (
     gameState.keys["ArrowDown"] &&
     gameState.paddle2Y < canvas.height - gameState.paddleHeight
   )
-    gameState.paddle2Y += gameState.paddleSpeed;
+    gameState.paddle2Y += gameState.paddleSpeed * deltaTime;
 }
