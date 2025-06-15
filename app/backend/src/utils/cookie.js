eec016f76bedc678b1cb1161fec5e71a0b78d29f
QCHR1581 <chris1581.at@gmail.com>
2025-06-15 05:53:33 +0000

    fix: remove arguments to specify cookie names

--- app/backend/src/utils/cookie.js
+++ app/backend/src/utils/cookie.js
@@ -1,12 +1,6 @@
 import env from "../config/env.js";
 
-export function setAuthCookies(
-  reply,
-  accessToken,
-  accessTokenCookieName,
-  refreshToken,
-  refreshTokenCookieName
-) {
+export function setAuthCookies(reply, accessToken, refreshToken) {
   const accessTokenTimeToExpire = new Date(
     Date.now() + parseInt(env.accessTokenTimeToExpireInMs)
   );
@@ -14,14 +8,14 @@ export function setAuthCookies(
     Date.now() + parseInt(env.refreshTokenTimeToExpireInMS)
   );
   return reply
-    .setCookie(accessTokenCookieName, accessToken, {
+    .setCookie("accessToken", accessToken, {
       httpOnly: true,
       secure: true,
       sameSite: "strict",
       path: "/",
       expires: accessTokenTimeToExpire
     })
-    .setCookie(refreshTokenCookieName, refreshToken, {
+    .setCookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
       sameSite: "strict",
