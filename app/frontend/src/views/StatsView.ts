e45330898c50a05bc2d8178d2857ac4e9a62bfc0
Gabriel Wolf <gwolf@student.42vienna.com>
2025-06-21 05:44:01 +0000

    refactor: move HTML creation into sub functions

--- app/frontend/src/views/StatsView.ts
+++ app/frontend/src/views/StatsView.ts
@@ -1,6 +1,13 @@
 import AbstractView from "./AbstractView.js";
-import { getUserStats } from "../services/userStatsServices.js";
-import { getUserMatches } from "../services/userServices.js";
+import {
+  getUserStats,
+  getUserStatsByUsername
+} from "../services/userStatsServices.js";
+import {
+  getUserByUsername,
+  getUserMatches,
+  getUserMatchesByUsername
+} from "../services/userServices.js";
 import { escapeHTML } from "../utility.js";
 import { auth } from "../AuthManager.js";
 import { Header1 } from "../components/Header1.js";
@@ -11,10 +18,13 @@ import { Match } from "../types/IMatch.js";
 import { router } from "../routing/Router.js";
 import { getUserFriendRequestByUsername } from "../services/friendsServices.js";
 import { FriendRequest } from "../types/FriendRequest.js";
+import { User } from "../types/User.js";
+import { UserStats } from "../types/IUserStats.js";
 
 export default class StatsView extends AbstractView {
   private viewType: "self" | "friend" | "public" = "public";
-  private username = router.getParams().username;
+  private username = escapeHTML(router.getParams().username);
+  private user: User | null = null;
   private friendRequest: FriendRequest | null = null;
 
   constructor() {
@@ -22,78 +32,83 @@ export default class StatsView extends AbstractView {
     this.setTitle("Stats");
   }
 
-  private userStatsHTML: string = "";
-  private matchesHTML: string[] = [];
+  private userStats: UserStats | null = null;
+  private matches: Match[] | null = null;
 
   createHTML() {
-    if (this.viewType === "self") {
-      return /* HTML */ `
-        <div class="w-full max-w-screen-2xl mx-auto px-4 py-8 space-y-8">
-          ${Header1({
-            text: "Player Statistics",
-            id: "player-statistics-header",
-            variant: "default"
-          })}
-          ${Table({
-            id: "user-stats-table",
-            headers: [
-              "Username",
-              "Total Matches",
-              "Matches Won",
-              "Matches Lost",
-              "Win Rate"
-            ],
-            rows: [this.userStatsHTML]
-          })}
-        </div>
-
-        <div class="w-full max-w-screen-2xl mx-auto px-4 py-8 space-y-8">
-          ${Header1({
-            text: "Match History",
-            id: "match-history-header",
-            variant: "default"
-          })}
-          ${Table({
-            id: "match-history-table",
-            headers: [
-              "Player1",
-              "Player1 Score",
-              "Player2",
-              "Player2 Score",
-              "Result",
-              "Date",
-              "Tournament"
-            ],
-            rows: this.matchesHTML
-          })}
-        </div>
-      `;
+    let html;
+    html = this.getUserStatsHTML();
+    if (this.viewType === "self" || this.viewType === "friend") {
+      html += this.getMatchesHTML();
     }
-    return "";
+    return html;
   }
 
   async render() {
     await this.setViewType();
-    this.userStatsHTML = await this.getUserStatsHTML();
-    this.matchesHTML = await this.getMatchesHTML();
+    await this.fetchData();
     this.updateHTML();
   }
 
-  async getUserStatsHTML(): Promise<string> {
-    const user: string = escapeHTML(auth.getToken().username);
-    const userStats = await getUserStats();
+  getUserStatsHTML(): string {
+    if (!this.userStats) throw new Error("UserStats is null");
 
-    return UserStatsRow(user, userStats);
+    const userStatsRow = UserStatsRow(this.username, this.userStats);
+
+    return /* HTML */ ` <div
+      class="w-full max-w-screen-2xl mx-auto px-4 py-8 space-y-8"
+    >
+      ${Header1({
+        text: "Player Statistics",
+        id: "player-statistics-header",
+        variant: "default"
+      })}
+      ${Table({
+        id: "user-stats-table",
+        headers: [
+          "Username",
+          "Total Matches",
+          "Matches Won",
+          "Matches Lost",
+          "Win Rate"
+        ],
+        rows: [userStatsRow]
+      })}
+    </div>`;
   }
 
-  async getMatchesHTML(): Promise<string[]> {
-    const user = escapeHTML(auth.getToken().username);
-    const matchesRaw = await getUserMatches();
+  getMatchesHTML(): string {
+    if (!this.matches) throw new Error("Matches is null");
 
-    if (matchesRaw.length === 0) {
-      return [NoMatchesRow()];
-    }
-    return matchesRaw.map((matchRaw: Match) => MatchRow(matchRaw, user));
+    const matchesRows =
+      this.matches.length === 0
+        ? [NoMatchesRow()]
+        : this.matches.map((matchRaw: Match) =>
+            MatchRow(matchRaw, this.username)
+          );
+
+    return /* HTML */ ` <div
+      class="w-full max-w-screen-2xl mx-auto px-4 py-8 space-y-8"
+    >
+      ${Header1({
+        text: "Match History",
+        id: "match-history-header",
+        variant: "default"
+      })}
+      ${Table({
+        id: "match-history-table",
+        headers: [
+          "Player1",
+          "Player1 Score",
+          "Player2",
+          "Player2 Score",
+          "Result",
+          "Date",
+          "Tournament"
+        ],
+        rows: matchesRows
+      })}
+    </div>`;
   }
 
   getName(): string {
@@ -105,11 +120,28 @@ export default class StatsView extends AbstractView {
       this.viewType = "self";
       return;
     }
+    this.user = await getUserByUsername(this.username);
+    if (!this.user) {
+      throw Error("User not found");
+    }
     this.friendRequest = await getUserFriendRequestByUsername(this.username);
     if (this.friendRequest?.status === "ACCEPTED") {
       this.viewType = "friend";
     } else {
       this.viewType = "public";
+    }
+  }
+
+  async fetchData() {
+    if (this.viewType === "self") {
+      this.userStats = await getUserStats();
+      this.matches = await getUserMatches();
+      return;
+    }
+    this.userStats = await getUserStatsByUsername(this.username);
+    if (!this.userStats) throw new Error("Could not fetch user-stats");
+    if (this.viewType === "friend") {
+      this.matches = await getUserMatchesByUsername(this.username);
     }
   }
 }
