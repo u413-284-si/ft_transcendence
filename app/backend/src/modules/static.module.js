049446f315327d22f34e5ee06c531d2574fa2808
Gabriel Wolf <gwolf@student.42vienna.com>
2025-06-25 18:08:27 +0000

    feat: prefix static routes, deactivate wildcard, implement cache control

--- app/backend/src/modules/static.module.js
+++ app/backend/src/modules/static.module.js
@@ -9,16 +9,34 @@ export default async function staticModule(fastify) {
   });
 
   await fastify.register(fastifyStatic, {
-    root: "/workspaces/ft_transcendence/app/frontend/public"
+    root: "/workspaces/ft_transcendence/app/frontend/public",
+    // By default all assets are immutable and can be cached for a long period due to cache bursting techniques
+    wildcard: false,
+    maxAge: "30d",
+    immutable: true,
+    prefix: "/static/"
+  });
+
+  // Explicitly reduce caching of assets that don't use cache bursting techniques
+  fastify.get("/", function (req, reply) {
+    // index.html should never be cached
+    reply.sendFile("index.html", {
+      root: "/workspaces/ft_transcendence/app/frontend/public",
+      maxAge: 0,
+      immutable: false
+    });
+  });
+
+  fastify.get("/favicon.ico", function (req, reply) {
+    // favicon can be cached for a short period
+    reply.sendFile("favicon.ico", {
+      root: "/workspaces/ft_transcendence/app/frontend/public",
+      maxAge: "1d",
+      immutable: false
+    });
   });
 
   fastify.setNotFoundHandler((req, reply) => {
-    if (req.raw.url && req.raw.url.startsWith("/api")) {
-      return reply.status(404).send({
-        message: `Fail: ${req.raw.url}`,
-        cause: "Route does not exist"
-      });
-    }
     return reply.status(200).sendFile("index.html");
   });
 }
