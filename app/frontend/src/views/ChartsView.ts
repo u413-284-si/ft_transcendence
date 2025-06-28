8c323d907712c8dbe2b9d12b8579f472211ef8b4
Gabriel Wolf <gwolf@student.42vienna.com>
2025-06-28 14:03:00 +0000

    feat: add win/losses

--- app/frontend/src/views/ChartsView.ts
+++ app/frontend/src/views/ChartsView.ts
@@ -1,38 +1,54 @@
+import { getUserStats } from "../services/userStatsServices.js";
+import { UserStats } from "../types/IUserStats.js";
 import AbstractView from "./AbstractView.js";
 
 export default class ChartsView extends AbstractView {
+  private userStats: UserStats | null = null;
+
   constructor() {
     super();
     this.setTitle("Charts");
   }
 
   createHTML() {
-    return /* HTML */ `<div id="chart"></div>`;
+    return /* HTML */ `<div id="chart" class="w-[300px] h-[300px]"></div>`;
   }
 
   async render() {
+    this.userStats = await getUserStats();
     this.updateHTML();
-    const options = {
-      chart: {
-        type: "line"
-      },
-      series: [
-        {
-          name: "sales",
-          data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-        }
-      ],
-      xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-      }
-    };
-
-    const chart = new ApexCharts(document.querySelector("#chart"), options);
-
-    chart.render();
+    this.createWinLossChart(this.userStats);
   }
 
   getName(): string {
     return "charts";
+  }
+
+  createWinLossChart(stats: UserStats) {
+    const options = {
+      chart: {
+        type: "pie",
+        fontFamily: "inherit",
+        background: "transparent"
+      },
+      labels: ["Wins", "Losses"],
+      series: [stats.matchesWon, stats.matchesLost],
+      colors: ["#00ffff", "#ff0044"],
+      title: {
+        text: "Wins vs Losses",
+        style: {
+          color: "#00ffff",
+          fontSize: "20px"
+        }
+      },
+      legend: {
+        labels: {
+          colors: ["#00ffff", "#00ffff"]
+        }
+      }
+    };
+
+    const chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
   }
 }
