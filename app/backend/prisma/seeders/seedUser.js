80a4a66a497e8ee9eaa7375a3accb119fbf5d4dd
Gabriel Wolf <gwolf@student.42vienna.com>
2025-06-30 16:15:10 +0000

    feat: always create admin user

--- app/backend/prisma/seeders/seedUser.js
+++ app/backend/prisma/seeders/seedUser.js
@@ -6,7 +6,21 @@ import { createHash } from "../../src/services/auth.services.js";
 export async function seedUsers(count = 10) {
   const users = [];
 
-  for (let i = 0; i < count; i++) {
+  const adminUsername = "admin";
+  const adminEmail = "admin@example.com";
+  const adminPassword = await createHash("123");
+  const authProvider = "LOCAL";
+
+  const adminUser = await createUser(
+    adminUsername,
+    adminEmail,
+    adminPassword,
+    authProvider
+  );
+
+  users.push(adminUser);
+
+  for (let i = 1; i < count; i++) {
     const username = faker.internet.username();
     const email = faker.internet.email();
     const hashedPassword = await createHash("123");
