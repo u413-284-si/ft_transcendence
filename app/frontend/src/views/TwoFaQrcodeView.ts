7735402bb67dda8aa0b2bb7e1690eb6864353399
QCHR1581 <chris1581.at@gmail.com>
2025-07-14 10:26:10 +0000

    feat: add textbox, qrcode and submit button to TwoFaQrcodeView

--- app/frontend/src/views/TwoFaQrcodeView.ts
+++ app/frontend/src/views/TwoFaQrcodeView.ts
@@ -5,19 +5,50 @@ import AbstractView from "./AbstractView.js";
 import { Button } from "../components/Button.js";
 import { Form } from "../components/Form.js";
 import { Paragraph } from "../components/Paragraph.js";
+import { TextBox } from "../components/TextBox.js";
+import { generateTwoFaQrcode } from "../services/authServices.js";
+import { Image } from "../components/Image.js";
+import { Input } from "../components/Input.js";
 
 export default class TwoFaQrcodeView extends AbstractView {
+  private qrCode: string = "";
   constructor() {
     super();
-    this.setTitle("Login");
+    this.setTitle("2FA QR Code");
   }
 
   createHTML() {
     return /* HTML */ `
       ${Form({
         children: [
-          Paragraph({
-            text: "You have to activate 2FA for your account. Please use an authenticator app (eg. Google Authenticator) to scan the QR code below:"
+          TextBox({
+            id: "two-fa-qr-code-info",
+            text: [
+              "You have to active 2FA for your account.",
+              "",
+              "Please use an authenticator app",
+              "to scan the QR code below."
+            ],
+            variant: "warning",
+            size: "sm"
+          }),
+          Image({
+            id: "two-fa-qr-code",
+            src: this.qrCode,
+            alt: "QR Code"
+          }),
+          Input({
+            id: "two-fa-qr-code-input",
+            label: "Enter code",
+            name: "two-fa-qr-code-input",
+            type: "text",
+            errorId: "two-fa-qr-code-input-error"
+          }),
+          Button({
+            text: "Submit",
+            variant: "default",
+            size: "md",
+            type: "submit"
           })
         ],
         id: "two-fa-form"
@@ -36,11 +67,16 @@ export default class TwoFaQrcodeView extends AbstractView {
   }
 
   async render() {
-    this.updateHTML();
+    await this.fetchData();
     this.addListeners();
+    this.updateHTML();
   }
 
   getName(): string {
-    return "two-fa";
+    return "two-fa-qrcode";
+  }
+
+  async fetchData() {
+    this.qrCode = await generateTwoFaQrcode();
   }
 }
