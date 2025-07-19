b71e8b6e726608b20904a963421e4ee47ea1a9f3
QCHR1581 <chris1581.at@gmail.com>
2025-07-19 16:23:08 +0000

    fix: remove 2fa setup during registration

--- app/frontend/src/views/RegisterView.ts
+++ app/frontend/src/views/RegisterView.ts
@@ -20,102 +20,63 @@ import { Image } from "../components/Image.js";
 import { generateTwoFaQrcode } from "../services/authServices.js";
 
 export default class Register extends AbstractView {
-  private qrCode: string = "";
   constructor() {
     super();
     this.setTitle("Register");
   }
 
   createHTML(): string {
-    return /* HTML */ ` ${Form({
-      children: [
-        Header1({
-          text: "Register Here",
-          variant: "default"
-        }),
-        Input({
-          id: "email",
-          label: "Email",
-          name: "email",
-          placeholder: "Email",
-          type: "email",
-          errorId: "email-error"
-        }),
-        Input({
-          id: "username",
-          label: "Username",
-          name: "username",
-          placeholder: "Username",
-          type: "text",
-          errorId: "username-error"
-        }),
-        Input({
-          id: "password",
-          label: "Password",
-          name: "password",
-          placeholder: "Password",
-          type: "password",
-          errorId: "password-error",
-          hasToggle: true
-        }),
-        Input({
-          id: "confirm",
-          label: "Confirm Password",
-          name: "confirm",
-          placeholder: "Confirm Password",
-          type: "password",
-          errorId: "confirm-error",
-          hasToggle: true
-        }),
-        Button({
-          text: "Register",
-          variant: "default",
-          size: "md",
-          type: "submit"
-        })
-      ],
-      id: "register-form"
-    })}
-    ${Modal({
-      id: "two-fa-modal",
-      children: [
-        Form({
-          children: [
-            TextBox({
-              id: "two-fa-qr-code-info",
-              text: [
-                "You have to active 2FA for your account.",
-                "",
-                "Please use an authenticator app",
-                "to scan the QR code below."
-              ],
-              variant: "warning",
-              size: "sm"
-            }),
-            Image({
-              id: "two-fa-qr-code",
-              src: this.qrCode,
-              alt: "QR Code"
-            }),
-            Input({
-              id: "two-fa-qr-code-input",
-              label: "Enter code",
-              name: "two-fa-qr-code-input",
-              type: "text",
-              errorId: "two-fa-qr-code-input-error"
-            }),
-            Button({
-              id: "two-fa-submit",
-              text: "Submit",
-              variant: "default",
-              size: "md",
-              type: "submit"
-            })
-          ],
-          id: "two-fa-form"
-        })
-      ]
-    })}`;
+    return /* HTML */ `
+      ${Form({
+        children: [
+          Header1({
+            text: "Register Here",
+            variant: "default"
+          }),
+          Input({
+            id: "email",
+            label: "Email",
+            name: "email",
+            placeholder: "Email",
+            type: "email",
+            errorId: "email-error"
+          }),
+          Input({
+            id: "username",
+            label: "Username",
+            name: "username",
+            placeholder: "Username",
+            type: "text",
+            errorId: "username-error"
+          }),
+          Input({
+            id: "password",
+            label: "Password",
+            name: "password",
+            placeholder: "Password",
+            type: "password",
+            errorId: "password-error",
+            hasToggle: true
+          }),
+          Input({
+            id: "confirm",
+            label: "Confirm Password",
+            name: "confirm",
+            placeholder: "Confirm Password",
+            type: "password",
+            errorId: "confirm-error",
+            hasToggle: true
+          }),
+          Button({
+            text: "Register",
+            variant: "default",
+            size: "md",
+            type: "submit"
+          })
+        ],
+        id: "register-form"
+      })}
+    `;
   }
 
   protected addListeners(): void {
@@ -170,24 +131,15 @@ export default class Register extends AbstractView {
       return;
     }
 
-    await this.fetchData(userEl.value);
-    console.log(this.qrCode);
-    const twoFaModal = getEl("two-fa-modal");
-    twoFaModal.classList.remove("hidden");
-
     try {
-      //   await registerUser(emailEL.value, userEl.value, passwordEl.value);
+      await registerUser(emailEL.value, userEl.value, passwordEl.value);
+      toaster.success(`Successfully registered ${userEl.value}!`);
+      router.navigate("/login", false);
     } catch (error) {
       if (error instanceof ApiError && error.status === 409) {
         toaster.error("Email or username already exists");
         return;
       }
     }
-  }
-
-  private async fetchData(username: string): Promise<void> {
-    this.qrCode = await generateTwoFaQrcode(username);
-    const twoFaQrcodeEl = getEl("two-fa-qr-code") as HTMLImageElement;
-    twoFaQrcodeEl.src = this.qrCode;
   }
 }
