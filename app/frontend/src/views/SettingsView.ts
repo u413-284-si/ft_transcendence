58e9d756e373bc61e2ca8b7dcc5e23fdb2c5f8c6
QCHR1581 <chris1581.at@gmail.com>
2025-07-21 08:12:54 +0000

    feat: adap 2fa setup modal depending on 2fa status

--- app/frontend/src/views/SettingsView.ts
+++ app/frontend/src/views/SettingsView.ts
@@ -8,7 +8,10 @@ import { TextBox } from "../components/TextBox.js";
 import { Input } from "../components/Input.js";
 import { Image } from "../components/Image.js";
 import { getEl } from "../utility.js";
-import { generateTwoFaQrcode } from "../services/authServices.js";
+import {
+  generateTwoFaQrcode,
+  getTwoFaStatus
+} from "../services/authServices.js";
 import { auth } from "../AuthManager.js";
 import { validateTwoFaCode } from "../validate.js";
 import { getDataOrThrow } from "../services/api.js";
@@ -16,6 +19,7 @@ import { toaster } from "../Toaster.js";
 
 export default class SettingsView extends AbstractView {
   private qrCode: string = "";
+  private hasTwoFa: boolean = false;
 
   constructor() {
     super();
@@ -44,37 +48,58 @@ export default class SettingsView extends AbstractView {
             children: [
               Form({
                 children: [
-                  TextBox({
-                    id: "two-fa-qr-code-info",
-                    text: [
-                      "Activate 2FA:",
-                      "",
-                      "please use an authenticator app",
-                      "to scan the QR code below."
-                    ],
-                    variant: "info",
-                    size: "sm"
-                  }),
+                  !this.hasTwoFa
+                    ? TextBox({
+                        id: "two-fa-qr-code-info",
+                        text: [
+                          "Activate 2FA:",
+                          "",
+                          "please use an authenticator app",
+                          "to scan the QR code below."
+                        ],
+                        variant: "info",
+                        size: "sm"
+                      })
+                    : TextBox({
+                        id: "two-fa-qr-code-info",
+                        text: [
+                          "Deactivate 2FA:",
+                          "",
+                          "By deactivating 2FA, you will remove your current 2FA setup."
+                        ],
+                        variant: "warning",
+                        size: "sm"
+                      }),
                   Image({
                     id: "two-fa-qr-code",
                     src: this.qrCode,
                     alt: "QR Code"
                   }),
-                  Input({
-                    id: "two-fa-qr-code-input",
-                    label: "Enter code",
-                    name: "two-fa-qr-code-input",
-                    type: "text",
-                    placeholder: "Code",
-                    errorId: "two-fa-qr-code-input-error"
-                  }),
-                  Button({
-                    id: "two-fa-submit",
-                    text: "Activate",
-                    variant: "default",
-                    size: "md",
-                    type: "submit"
-                  })
+                  !this.hasTwoFa
+                    ? Input({
+                        id: "two-fa-qr-code-input",
+                        label: "Enter code",
+                        name: "two-fa-qr-code-input",
+                        type: "text",
+                        placeholder: "Code",
+                        errorId: "two-fa-qr-code-input-error"
+                      })
+                    : "",
+                  !this.hasTwoFa
+                    ? Button({
+                        id: "two-fa-submit",
+                        text: "Activate",
+                        variant: "default",
+                        size: "md",
+                        type: "submit"
+                      })
+                    : Button({
+                        id: "two-fa-remove",
+                        text: "Deactivate",
+                        variant: "danger",
+                        size: "md",
+                        type: "submit"
+                      })
                 ],
                 id: "two-fa-form"
               })
@@ -100,8 +125,9 @@ export default class SettingsView extends AbstractView {
   }
 
   async render() {
-    this.updateHTML();
     await this.fetchData();
+    this.updateHTML();
+    this.setData();
     this.addListeners();
   }
 
@@ -150,6 +176,10 @@ export default class SettingsView extends AbstractView {
 
   private async fetchData(): Promise<void> {
     this.qrCode = getDataOrThrow(await generateTwoFaQrcode());
+    this.hasTwoFa = getDataOrThrow(await getTwoFaStatus()).hasTwoFa;
+  }
+
+  private setData() {
     const twoFaQrcodeEl = getEl("two-fa-qr-code") as HTMLImageElement;
     twoFaQrcodeEl.src = this.qrCode;
   }
