98df96912af443c9ee629f7ee42c08751baedd17
QCHR1581 <chris1581.at@gmail.com>
2025-07-27 12:55:31 +0000

    refactor: remove two fa replaceholder

--- app/frontend/src/views/SettingsView.ts
+++ app/frontend/src/views/SettingsView.ts
@@ -26,121 +26,111 @@ import { router } from "../routing/Router.js";
 export default class SettingsView extends AbstractView {
   private hasTwoFa: boolean = false;
   private hasLocalAuth: boolean = auth.getUser().authProvider === "LOCAL";
+  private isTwoFaGoingToBeRemoved: boolean = false;
 
   constructor() {
     super();
     this.setTitle("Settings");
   }
 
-  private async get2FaSetupHTML(): Promise<string | undefined> {
-    try {
-      return /* HTML */ `
-        ${Button({
-          id: "setup-two-fa-button",
-          text: "Setup 2FA",
-          variant: "default",
-          type: "button"
-        })}
-        ${Modal({
-          id: "two-fa-modal",
-          idCloseButton: "close-two-fa-modal-button",
-          children: [
-            Form({
-              children: [
-                !this.hasTwoFa
-                  ? TextBox({
-                      id: "two-fa-qr-code-info",
-                      text: [
-                        "Activate 2FA:",
-                        "",
-                        "please use an authenticator app",
-                        "to scan the QR code below."
-                      ],
-                      variant: "info",
-                      size: "sm"
-                    })
-                  : TextBox({
-                      id: "two-fa-qr-code-info",
-                      text: ["2FA activated"],
-                      variant: "info",
-                      size: "sm"
-                    }),
-                Image({
-                  id: "two-fa-qr-code",
-                  src: getDataOrThrow(await generateTwoFaQrcode()).qrcode,
-                  alt: "QR Code"
-                }),
-                !this.hasTwoFa
-                  ? Input({
-                      id: "two-fa-qr-code-input",
-                      label: "Enter code",
-                      name: "two-fa-qr-code-input",
-                      type: "text",
-                      placeholder: "Code",
-                      errorId: "two-fa-qr-code-input-error"
-                    })
-                  : "",
-                !this.hasTwoFa
-                  ? Button({
-                      id: "two-fa-submit",
-                      text: "Activate",
-                      variant: "default",
-                      size: "md",
-                      type: "submit"
-                    })
-                  : Button({
-                      id: "two-fa-remove",
-                      text: "Deactivate",
-                      variant: "danger",
-                      size: "md",
-                      type: "submit"
-                    })
-              ],
-              id: "two-fa-form"
-            })
-          ]
-        })}
-        ${Modal({
-          id: "two-fa-password-modal",
-          idCloseButton: "close-two-fa-password-modal-button",
-          children: [
-            Form({
-              id: "two-fa-password-form",
-              children: [
-                Input({
-                  id: "two-fa-password-input",
-                  label: "Password",
-                  name: "two-fa-password-input",
-                  placeholder: "Password",
-                  type: "password",
-                  errorId: "two-fa-password-input-error",
-                  hasToggle: true
-                }),
-                Button({
-                  id: "two-fa-submit-password",
-                  text: "Confirm",
-                  variant: "default",
-                  size: "md",
-                  type: "submit"
-                })
-              ]
-            })
-          ]
-        })}
-      `;
-    } catch (error) {
-      router.handleError("Error getting 2FA setup", error);
-    }
-  }
-
-  private createTwoFaPlaceholder(): string {
-    return /* HTML */ ` <div id="two-fa-placeholder"></div> `;
+  private get2FaSetupHTML(): string {
+    return /* HTML */ `
+      ${Button({
+        id: "setup-two-fa-button",
+        text: "Setup 2FA",
+        variant: "default",
+        type: "button"
+      })}
+      ${Modal({
+        id: "two-fa-modal",
+        idCloseButton: "close-two-fa-modal-button",
+        children: [
+          Form({
+            children: [
+              !this.hasTwoFa
+                ? TextBox({
+                    id: "two-fa-qr-code-info",
+                    text: [
+                      "Activate 2FA:",
+                      "",
+                      "please use an authenticator app",
+                      "to scan the QR code below."
+                    ],
+                    variant: "info",
+                    size: "sm"
+                  })
+                : TextBox({
+                    id: "two-fa-qr-code-info",
+                    text: ["2FA activated"],
+                    variant: "info",
+                    size: "sm"
+                  }),
+              Image({
+                id: "two-fa-qr-code",
+                src: "",
+                alt: "QR Code"
+              }),
+              !this.hasTwoFa
+                ? Input({
+                    id: "two-fa-qr-code-input",
+                    label: "Enter code",
+                    name: "two-fa-qr-code-input",
+                    type: "text",
+                    placeholder: "Code",
+                    errorId: "two-fa-qr-code-input-error"
+                  })
+                : "",
+              !this.hasTwoFa
+                ? Button({
+                    id: "two-fa-submit",
+                    text: "Activate",
+                    variant: "default",
+                    size: "md",
+                    type: "submit"
+                  })
+                : Button({
+                    id: "two-fa-remove",
+                    text: "Deactivate",
+                    variant: "danger",
+                    size: "md",
+                    type: "submit"
+                  })
+            ],
+            id: "two-fa-form"
+          })
+        ]
+      })}
+      ${Modal({
+        id: "two-fa-password-modal",
+        idCloseButton: "close-two-fa-password-modal-button",
+        children: [
+          Form({
+            id: "two-fa-password-form",
+            children: [
+              Input({
+                id: "two-fa-password-input",
+                label: "Password",
+                name: "two-fa-password-input",
+                placeholder: "Password",
+                type: "password",
+                errorId: "two-fa-password-input-error",
+                hasToggle: true
+              }),
+              Button({
+                id: "two-fa-submit-password",
+                text: "Confirm",
+                variant: "default",
+                size: "md",
+                type: "submit"
+              })
+            ]
+          })
+        ]
+      })}
+    `;
   }
 
   createHTML() {
-    const twoFaPlaceholder = this.hasLocalAuth
-      ? this.createTwoFaPlaceholder()
-      : "";
     return /* HTML */ `
       <div class="text-center space-y-4">
         ${Header1({
@@ -151,7 +141,7 @@ export default class SettingsView extends AbstractView {
           text: "Configure your preferences and settings here.",
           id: "settings-intro"
         })}
-		${twoFaPlaceholder}
+		${this.hasLocalAuth ? this.get2FaSetupHTML() : ""}
         </div>
       </div>
     `;
@@ -160,27 +150,34 @@ export default class SettingsView extends AbstractView {
   protected addListeners(): void {
     document
       .getElementById("setup-two-fa-button")
-      ?.addEventListener("click", () => this.displayTwoFaSetupModal());
+      ?.addEventListener("click", () => this.displayTwoFaPasswordModal());
+    document
+      .getElementById("two-fa-password-form")
+      ?.addEventListener("submit", (event) =>
+        this.callPasswordFormAction(event)
+      );
     document
       .getElementById("close-two-fa-modal-button")
       ?.addEventListener("click", () => this.hideTwoFaSetupModal());
-    if (this.hasTwoFa) {
-      document
-        .getElementById("close-two-fa-password-modal-button")
-        ?.addEventListener("click", () => this.hideTwoFaPasswordModal());
-      document
-        .getElementById("two-fa-password-form")
-        ?.addEventListener("submit", (event) => this.removeTwoFa(event));
-    }
+    document
+      .getElementById("close-two-fa-password-modal-button")
+      ?.addEventListener("click", () => this.hideTwoFaPasswordModal());
     document
       .getElementById("two-fa-form")
       ?.addEventListener("submit", (event) => this.twoFaAction(event));
   }
 
+  private callPasswordFormAction(event: Event): void {
+    if (this.isTwoFaGoingToBeRemoved) {
+      this.removeTwoFa(event);
+    } else {
+      this.displayTwoFaSetup(event);
+    }
+  }
+
   async render() {
     await this.fetchData();
     this.updateHTML();
-    await this.setData();
     this.addListeners();
   }
 
