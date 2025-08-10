0a3d18e87bfc413373bcc261c5e53bbac244ffd4
Gabriel Wolf <gwolf@student.42vienna.com>
2025-08-10 15:19:28 +0000

    feat: options builder for ScoreDiff

---
+++ app/frontend/src/charts/matchesScoreDiffOptions.ts
@@ -0,0 +1,75 @@
+import type { ApexOptions } from "apexcharts";
+import { chartColors, toAxisSeries } from "./chartUtils.js";
+import { formatDateTime } from "../formatDate.js";
+
+export function buildMatchesScoreDiffOptions(
+  name: string,
+  data: { x: string; y: number }[],
+  totalMatches: number
+): ApexOptions {
+  const startingPoint = Math.max(totalMatches - data.length + 1, 0);
+  const transformedData = data.map((match, i) => ({
+    x: `# ${startingPoint + i}`,
+    y: match.y
+  }));
+
+  const datetimeLabels = data.map((match) => formatDateTime(match.x));
+
+  const series = toAxisSeries(name, transformedData);
+
+  const options: ApexOptions = {
+    chart: {
+      type: "bar",
+      fontFamily: "inherit",
+      background: "transparent",
+      toolbar: {
+        show: false
+      },
+      height: 300
+    },
+    legend: {
+      show: false
+    },
+    plotOptions: {
+      bar: {
+        distributed: true,
+        colors: {
+          ranges: [
+            {
+              from: -100,
+              to: -1,
+              color: chartColors.red
+            },
+            {
+              from: 0,
+              to: 100,
+              color: chartColors.cyan
+            }
+          ]
+        }
+      }
+    },
+    series: series,
+    tooltip: {
+      theme: "dark",
+      x: {
+        formatter: (_val, opts) => datetimeLabels[opts.dataPointIndex]
+      },
+      y: {
+        formatter: (val: number) => `${val > 0 ? "+" : ""}${val}`
+      }
+    },
+    xaxis: {
+      type: "category",
+      labels: { style: { colors: chartColors.white } }
+    },
+    yaxis: {
+      title: {
+        text: name,
+        style: { color: chartColors.white }
+      },
+      labels: { style: { colors: chartColors.white } }
+    }
+  };
+  return options;
+}
