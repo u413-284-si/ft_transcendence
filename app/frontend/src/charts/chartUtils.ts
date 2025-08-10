abcfd63acae6729859a395958261dbb5cb9473a3
Gabriel Wolf <gwolf@student.42vienna.com>
2025-08-10 14:39:47 +0000

    feat: add utils for chart handling

---
+++ app/frontend/src/charts/chartUtils.ts
@@ -0,0 +1,33 @@
+import type { ApexOptions } from "apexcharts";
+import { getEl } from "../utility.js";
+
+export async function renderChart(
+  id: string,
+  options: ApexOptions
+): Promise<ApexCharts> {
+  const chartEl = getEl(id);
+
+  const chart = new ApexCharts(chartEl, options);
+  await chart.render();
+  return chart;
+}
+
+export function toAxisSeries(
+  name: string,
+  data: { x: string | Date; y: number }[]
+): ApexAxisChartSeries {
+  return [{ name, data }];
+}
+
+function getColorFromCSSProperty(property: string): string {
+  return getComputedStyle(document.documentElement).getPropertyValue(property);
+}
+
+export const chartColors = {
+  white: getColorFromCSSProperty("--color-white-hex"),
+  cyan: getColorFromCSSProperty("--color-neon-cyan-hex"),
+  red: getColorFromCSSProperty("--color-neon-red-hex"),
+  green: getColorFromCSSProperty("--color-neon-green-hex"),
+  yellow: getColorFromCSSProperty("--color-neon-yellow-hex"),
+  purple: getColorFromCSSProperty("--color-neon-purple-hex")
+};
