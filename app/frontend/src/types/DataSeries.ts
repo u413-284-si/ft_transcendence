2d63b19e4984a26fd9ee952dd0d64e8cc8aeab03
Gabriel Wolf <gwolf@student.42vienna.com>
2025-08-11 13:59:29 +0000

    feat: add types for tournament dashboard data

--- app/frontend/src/types/DataSeries.ts
+++ app/frontend/src/types/DataSeries.ts
@@ -1,3 +1,5 @@
+import { TournamentSize } from "./ITournament";
+
 type DataPoint = {
   x: string;
   y: number;
@@ -9,4 +11,50 @@ export type DashboardMatches = {
   winrate: DataSeries;
   scoreDiff: DataSeries;
   scores: DataSeries;
+};
+
+type TournamentSummaryPoint = {
+  size: number;
+  winrate: number;
+  played: number;
+  won: number;
+};
+
+export type TournamentSummaryData = {
+  data: TournamentSummaryPoint[];
+};
+
+type TournamentProgressPoint = {
+  x: number;
+  y: number;
+};
+
+type TournamentProgressSeries = TournamentProgressPoint[];
+
+type TournamentProgressData = {
+  [K in TournamentSize]: TournamentProgressSeries;
+};
+
+type TournamentDayPoint = {
+  x: string;
+  y: number;
+};
+
+export type TournamentDaySeries = {
+  name: "win" | "loss";
+  data: TournamentDayPoint[];
+};
+
+export type TournamentDayData = {
+  [K in TournamentSize]: TournamentDaySeries[];
+};
+
+export type TournamentPlayedSeries = {
+  [K in TournamentSize]: TournamentDayPoint[];
+};
+
+export type DashboardTournaments = {
+  summary: TournamentSummaryData;
+  progress: TournamentProgressData;
+  lastNDays: TournamentDayData;
 };
