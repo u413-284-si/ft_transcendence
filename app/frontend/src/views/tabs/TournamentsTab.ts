e50d8a075ac4da53e23c63477c25427180f336cc
Gabriel Wolf <gwolf@student.42vienna.com>
2025-08-11 14:02:03 +0000

    feat: add tournaments tab

---
+++ app/frontend/src/views/tabs/TournamentsTab.ts
@@ -0,0 +1,133 @@
+import { buildTournamentsPlayedOptions } from "../../charts/tournamentsPlayedOptions.js";
+import { buildTournamentsProgressOptions } from "../../charts/tournamentsProgressOptions.js";
+import { buildTournamentsLastTenDaysOptions } from "../../charts/tournamentsLastTenDaysOptions.js";
+import { buildTournamentsSummaryOptions } from "../../charts/tournamentsSummaryOptions.js";
+import { Chart } from "../../components/Chart.js";
+import { Header1 } from "../../components/Header1.js";
+import { getDataOrThrow } from "../../services/api.js";
+import { getUserDashboardTournamentsByUsername } from "../../services/userStatsServices.js";
+import { DashboardTournaments } from "../../types/DataSeries.js";
+import { AbstractTab } from "./AbstractTab.js";
+
+export class TournamentsTab extends AbstractTab {
+  private dashboard: DashboardTournaments | null = null;
+  private username: string;
+
+  constructor(username: string) {
+    super();
+    this.username = username;
+  }
+
+  getHTML(): string {
+    return /* HTML */ ` <div id="tab-tournaments">
+      <div class="w-full max-w-screen-2xl mx-auto px-4 py-8 space-y-8">
+        ${Header1({
+          text: i18next.t("statsView.dashboard"),
+          id: "tournament-dashboard-header",
+          variant: "default"
+        })}
+        ${this.getDashboardHTML()}
+      </div>
+      <div class="w-full max-w-screen-2xl mx-auto px-4 py-8 space-y-8">
+        ${Header1({
+          text: i18next.t("statsView.details"),
+          id: "tournament-details-header",
+          variant: "default"
+        })}
+      </div>
+    </div>`;
+  }
+
+  getDashboardHTML(): string {
+    const rangeDays = i18next.t("chart.rangeLastDays", { count: 10 });
+
+    return /* HTML */ `<div class="p-6 mx-auto space-y-8">
+      <div class="flex gap-8">
+        ${Chart({
+          title: i18next.t("chart.summary"),
+          chartId: "tournament-summary"
+        })}
+        ${Chart({
+          title: i18next.t("chart.played", { range: rangeDays }),
+          chartId: "tournament-played"
+        })}
+      </div>
+      <div class="flex gap-8">
+        ${Chart({
+          title: i18next.t("chart.winLoss", { range: rangeDays }),
+          chartId: "tournament-last-10-days-4"
+        })}
+        ${Chart({
+          title: i18next.t("chart.progress", { num: 4 }),
+          chartId: "tournament-progress-4"
+        })}
+      </div>
+      <div class="flex gap-8">
+        ${Chart({
+          title: i18next.t("chart.winLoss", { range: rangeDays }),
+          chartId: "tournament-last-10-days-8"
+        })}
+        ${Chart({
+          title: i18next.t("chart.progress", { num: 8 }),
+          chartId: "tournament-progress-8"
+        })}
+      </div>
+      <div class="flex gap-8">
+        ${Chart({
+          title: i18next.t("chart.winLoss", { range: rangeDays }),
+          chartId: "tournament-last-10-days-16"
+        })}
+        ${Chart({
+          title: i18next.t("chart.progress", { num: 16 }),
+          chartId: "tournament-progress-16"
+        })}
+      </div>
+    </div>`;
+  }
+
+  async init(): Promise<void> {
+    this.dashboard = getDataOrThrow(
+      await getUserDashboardTournamentsByUsername(this.username)
+    );
+    this.populateChartOptions();
+    this.isInit = true;
+  }
+
+  private populateChartOptions(): void {
+    if (!this.dashboard) throw new Error(i18next.t("error.somethingWentWrong"));
+
+    this.chartOptions = {
+      "tournament-summary": buildTournamentsSummaryOptions(
+        i18next.t("chart.summary"),
+        this.dashboard.summary
+      ),
+      "tournament-played": buildTournamentsPlayedOptions(
+        this.dashboard.lastNDays
+      ),
+      "tournament-last-10-days-4": buildTournamentsLastTenDaysOptions(
+        this.dashboard.lastNDays[4],
+        4
+      ),
+      "tournament-progress-4": buildTournamentsProgressOptions(
+        this.dashboard.progress[4].reverse(),
+        4
+      ),
+      "tournament-last-10-days-8": buildTournamentsLastTenDaysOptions(
+        this.dashboard.lastNDays[8],
+        8
+      ),
+      "tournament-progress-8": buildTournamentsProgressOptions(
+        this.dashboard.progress[8].reverse(),
+        8
+      ),
+      "tournament-last-10-days-16": buildTournamentsLastTenDaysOptions(
+        this.dashboard.lastNDays[16],
+        16
+      ),
+      "tournament-progress-16": buildTournamentsProgressOptions(
+        this.dashboard.progress[16].reverse(),
+        16
+      )
+    };
+  }
+}
