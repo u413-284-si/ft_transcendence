62f798d4bbd0d0d09d60f4abb7bd8588c801de6e
QCHR1581 <chris1581.at@gmail.com>
2025-08-12 20:14:53 +0000

    refactor: introduce new functionalities to Link component

--- app/frontend/src/components/Link.ts
+++ app/frontend/src/components/Link.ts
@@ -1,26 +1,31 @@
 export type LinkOptions = {
   text?: string;
-  variant?: "default" | "empty";
+  variant?: "navigation" | "download";
   href?: string;
+  filename?: string;
   id?: string;
   className?: string;
 };
 
 const linkVariants: Record<string, string> = {
-  default:
+  navigation:
     "text-xl px-3 py-1 rounded-md text-teal hover:text-neon-orange hover:animate-glow-orange hover:font-semibold focus:outline-none focus-visible:ring-2 focus-visible:ring-neon-cyan",
-  empty: ""
+  download:
+    "px-4 py-2 text-base inline-flex items-center justify-center rounded-md font-bold focus:outline-none focus-visible:ring-2 focus-visible:ring-neon-cyan uppercase border border-neon-cyan text-white hover:shadow-neon-cyan hover:bg-neon-cyan transition-all duration-500 ease-in-out"
 };
 
 export function Link({
   text = "",
-  variant = "default",
+  variant = "navigation",
   href = "#",
+  filename = "",
   id,
   className = ""
 }: LinkOptions): string {
   // Merge all classes
   const classes = [linkVariants[variant], className].join(" ");
+  const linkAttr = variant === "navigation" ? "data-link" : "";
+  const downloadAttr = variant === "download" ? `download="${filename}"` : "";
 
-  return `<a${id ? ` id="${id}"` : ""} href="${href}" class="${classes}" data-link>${text}</a>`;
+  return `<a${id ? ` id="${id}"` : ""} href="${href}" class="${classes}" ${linkAttr}${downloadAttr}>${text}</a>`;
 }
