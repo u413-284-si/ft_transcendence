186bf2279bfd1b041d4f423ad02066b2c5f198b6
QCHR1581 <chris1581.at@gmail.com>
2025-08-12 18:20:30 +0000

    fix: fix getName return value

--- app/frontend/src/views/TwoFABackupCodeVerifyView.ts
+++ app/frontend/src/views/TwoFABackupCodeVerifyView.ts
@@ -56,7 +56,7 @@ export default class TwoFABackupCodeVerifyView extends AbstractView {
   }
 
   getName(): string {
-    return "login";
+    return "twoFABackupCodeVerification";
   }
 
   private async verifTwoFABackupCode(event: Event) {
