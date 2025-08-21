f6d53808dbc7394e0787276589b7660e3549b6d9
Gabriel Wolf <gwolf@student.42vienna.com>
2025-08-21 11:44:15 +0000

    feat: add transaction for tournament

--- app/backend/src/services/transactions.services.js
+++ app/backend/src/services/transactions.services.js
@@ -1,5 +1,6 @@
 import prisma from "../prisma/prismaClient.js";
 import { createMatchTx } from "./matches.services.js";
+import { generateBracket } from "./tournaments.services.js";
 import { updateUserStatsTx } from "./user_stats.services.js";
 
 export async function transactionMatch(
@@ -40,5 +41,48 @@ export async function transactionMatch(
       );
     }
     return { match, stats };
+  });
+}
+
+export async function transactionTournament(
+  name,
+  maxPlayers,
+  userId,
+  userNickname,
+  nicknames,
+  playertypes
+) {
+  return prisma.$transaction(async (tx) => {
+    const tournament = await tx.tournament.create({
+      data: {
+        name,
+        maxPlayers,
+        userId,
+        userNickname
+      }
+    });
+
+    const bracket = generateBracket(nicknames, playertypes, maxPlayers);
+
+    const bracketData = bracket.map((b) => ({
+      tournamentId: tournament.id,
+      matchNumber: b.matchNumber,
+      round: b.round,
+      player1Nickname: b.player1Nickname,
+      player2Nickname: b.player2Nickname,
+      player1Type: b.player1Type,
+      player2Type: b.player2Type,
+      winner: null,
+      nextMatchNumber: b.nextMatchNumber,
+      winnerSlot: b.winnerSlot
+    }));
+
+    await tx.bracketMatch.createMany({
+      data: bracketData
+    });
+
+    tournament.bracket = bracket;
+
+    return tournament;
   });
 }
