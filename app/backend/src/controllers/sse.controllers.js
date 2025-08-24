7582498f8de1616e822ecac3d4040ba21cae767f
QCHR1581 <chris1581.at@gmail.com>
2025-08-24 15:54:25 +0000

    refactor: remove try and catch blocks from sse controllers

--- app/backend/src/controllers/sse.controllers.js
+++ app/backend/src/controllers/sse.controllers.js
@@ -1,28 +1,16 @@
 import { registerSSEConnection } from "../services/events/sse.services.js";
-import { handlePrismaError } from "../utils/error.js";
+import { handleError } from "../utils/error.js";
 import { createResponseMessage } from "../utils/response.js";
 
 export async function sseConnectionHandler(request, reply) {
-  const action = "SSE Connection";
-  try {
-    const userId = request.user.id;
+  request.action = "SSE Connection";
+  const userId = request.user.id;
 
-    // Setup SSE headers
-    reply.raw.setHeader("Content-Type", "text/event-stream");
-    reply.raw.setHeader("Cache-Control", "no-cache");
-    reply.raw.setHeader("Connection", "keep-alive");
-    reply.raw.flushHeaders();
+  // Setup SSE headers
+  reply.raw.setHeader("Content-Type", "text/event-stream");
+  reply.raw.setHeader("Cache-Control", "no-cache");
+  reply.raw.setHeader("Connection", "keep-alive");
+  reply.raw.flushHeaders();
 
-    await registerSSEConnection(userId, reply);
-  } catch (err) {
-    request.log.error(
-      { err, body: request.body },
-      `sseOnlineHandler: ${createResponseMessage(action, false)}`
-    );
-    if (!reply.raw.headersSent) {
-      return handlePrismaError(reply, action, err);
-    } else {
-      reply.raw.end();
-    }
-  }
+  await registerSSEConnection(userId, reply);
 }
