3c9ab6b8b581ed833b24ec1fc1530fc055e6f0ec
QCHR1581 <chris1581.at@gmail.com>
2025-08-24 15:40:55 +0000

    remove try and catch blocks from tournaments controllers

--- app/backend/src/controllers/tournaments.controllers.js
+++ app/backend/src/controllers/tournaments.controllers.js
@@ -7,115 +7,68 @@ import {
   deleteTournament
 } from "../services/tournaments.services.js";
 import { createResponseMessage } from "../utils/response.js";
-import { handlePrismaError } from "../utils/error.js";
 
 export async function createTournamentHandler(request, reply) {
-  const action = "Create tournament";
-  try {
-    const userId = request.user.id;
-    const { name, maxPlayers, userNickname, bracket } = request.body;
+  request.action = "Create tournament";
+  const userId = request.user.id;
+  const { name, maxPlayers, userNickname, bracket } = request.body;
 
-    const data = await createTournament(
-      name,
-      maxPlayers,
-      userId,
-      userNickname,
-      bracket
-    );
-    return reply
-      .code(201)
-      .send({ message: createResponseMessage(action, true), data: data });
-  } catch (err) {
-    request.log.error(
-      { err, body: request.body },
-      `createTournamentHandler: ${createResponseMessage(action, false)}`
-    );
-    handlePrismaError(reply, action, err);
-  }
+  const data = await createTournament(
+    name,
+    maxPlayers,
+    userId,
+    userNickname,
+    bracket
+  );
+  return reply
+    .code(201)
+    .send({ message: createResponseMessage(request.action, true), data: data });
 }
 
 export async function getAllTournamentsHandler(request, reply) {
-  const action = "Get all tournaments";
-  try {
-    const data = await getAllTournaments();
-    const count = data.length;
-    return reply
-      .code(200)
-      .send({ message: createResponseMessage(action, true), count, data });
-  } catch (err) {
-    request.log.error(
-      { err, body: request.body },
-      `getAllTournaments: ${createResponseMessage(action, false)}`
-    );
-    handlePrismaError(reply, action, err);
-  }
+  request.action = "Get all tournaments";
+  const data = await getAllTournaments();
+  const count = data.length;
+  return reply.code(200).send({
+    message: createResponseMessage(request.action, true),
+    count,
+    data
+  });
 }
 
 export async function getTournamentHandler(request, reply) {
-  const action = "Get tournament";
-  try {
-    const tournamentId = request.params.id;
-    const data = await getTournament(tournamentId);
-    return reply
-      .code(200)
-      .send({ message: createResponseMessage(action, true), data: data });
-  } catch (err) {
-    request.log.error(
-      { err, body: request.body },
-      `getTournamentHandler: ${createResponseMessage(action, false)}`
-    );
-    handlePrismaError(reply, action, err);
-  }
+  request.action = "Get tournament";
+  const tournamentId = request.params.id;
+  const data = await getTournament(tournamentId);
+  return reply
+    .code(200)
+    .send({ message: createResponseMessage(request.action, true), data: data });
 }
 
 export async function patchTournamentHandler(request, reply) {
-  const action = "Patch tournament";
-  try {
-    const tournamentId = request.params.id;
-    const userId = request.user.id;
-    const data = await updateTournament(tournamentId, userId, request.body);
-    return reply
-      .code(200)
-      .send({ message: createResponseMessage(action, true), data: data });
-  } catch (err) {
-    request.log.error(
-      { err, body: request.body },
-      `patchTournamentHandler: ${createResponseMessage(action, false)}`
-    );
-    return handlePrismaError(reply, action, err);
-  }
+  request.action = "Patch tournament";
+  const tournamentId = request.params.id;
+  const userId = request.user.id;
+  const data = await updateTournament(tournamentId, userId, request.body);
+  return reply
+    .code(200)
+    .send({ message: createResponseMessage(request.action, true), data: data });
 }
 
 export async function deleteAllTournamentsHandler(request, reply) {
-  const action = "Delete all tournaments";
-  try {
-    const data = await deleteAllTournaments();
-    return reply
-      .code(200)
-      .send({ message: createResponseMessage(action, true), data: data });
-  } catch (err) {
-    request.log.error(
-      { err, body: request.body },
-      `deleteAllTournamentsHandler: ${createResponseMessage(action, false)}`
-    );
-    handlePrismaError(reply, action, err);
-  }
+  request.action = "Delete all tournaments";
+  const data = await deleteAllTournaments();
+  return reply
+    .code(200)
+    .send({ message: createResponseMessage(request.action, true), data: data });
 }
 
 export async function deleteTournamentHandler(request, reply) {
-  const action = "Delete Tournament";
-  try {
-    const tournamentId = request.params.id;
-    const userId = request.user.id;
-    const data = await deleteTournament(tournamentId, userId);
-    return reply
-      .code(200)
-      .send({ message: createResponseMessage(action, true), data: data });
-  } catch (err) {
-    request.log.error(
-      { err, body: request.body },
-      `deleteTournamentHandler: ${createResponseMessage(action, false)}`
-    );
-    return handlePrismaError(reply, action, err);
-  }
+  request.action = "Delete Tournament";
+  const tournamentId = request.params.id;
+  const userId = request.user.id;
+  const data = await deleteTournament(tournamentId, userId);
+  return reply
+    .code(200)
+    .send({ message: createResponseMessage(request.action, true), data: data });
 }
