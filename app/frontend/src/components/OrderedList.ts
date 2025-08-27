35469dd6fa8c5140d722fd6de63a28bfd4520954
Gabriel Wolf <gwolf@students.42vienna.com>
2025-08-27 19:32:52 +0000

    fix: text size

--- app/frontend/src/components/OrderedList.ts
+++ app/frontend/src/components/OrderedList.ts
@@ -7,7 +7,7 @@ export type OrderedListOptions = {
 
 export function OrderedList({
   children,
-  className = "text-md list-decimal list-outside space-y-1",
+  className = "text-md text-grey list-decimal list-outside space-y-1",
   id = "",
   start
 }: OrderedListOptions): string {
