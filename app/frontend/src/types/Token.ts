46696857b5c697ed8b746e0329a80c10b5edff3f
Gabriel Wolf <gwolf@student.42vienna.com>
2025-08-28 19:39:51 +0000

    feat: add tokeType and new type valid token

--- app/frontend/src/types/Token.ts
+++ app/frontend/src/types/Token.ts
@@ -1,10 +1,14 @@
 type TokenStatus = "valid" | "none" | "invalid" | "expired";
+type TokenType = "access" | "refresh";
 
 export type Token =
   | {
       status: "valid";
+      type: TokenType;
       exp: number;
     }
   | {
       status: Exclude<TokenStatus, "valid">;
     };
+
+export type ValidToken = Extract<Token, { status: "valid" }>;
