6f3fb9515e16e4c14769098c48db7ca641028ab3
Gabriel Wolf <gwolf@c3r1p1.42vienna.com>
2025-09-06 18:27:36 +0200

    fix: delete not needed files

--- app/backend/prisma/migrations/20250724193731_add_backup_code_table_to_authentication_table/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "BackupCode" (
-    "userId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "backupCode" TEXT,
-    CONSTRAINT "BackupCode_userId_fkey" FOREIGN KEY ("userId") REFERENCES "authentications" ("userId") ON DELETE CASCADE ON UPDATE CASCADE
-);
