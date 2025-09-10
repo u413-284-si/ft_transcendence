15bad8a16024b06b0e41cb4de8139128723d23ee
Gabriel Wolf <gwolf@students.42vienna.com>
2025-09-10 08:48:12 +0000

    fix: replace appLogger with viewLogger in tabs

--- app/frontend/src/views/tabs/FriendsTab.ts
+++ app/frontend/src/views/tabs/FriendsTab.ts
@@ -5,7 +5,7 @@ import { buildFriendsWinRateOptions } from "../../charts/friendsWinRateOptions.j
 import { buildFriendsWinstreakOptions } from "../../charts/friendsWinStreakOptions.js";
 import { Chart } from "../../components/Chart.js";
 import { Header1 } from "../../components/Header1.js";
-import { appLogger } from "../../logging/config.js";
+import { viewLogger } from "../../logging/config.js";
 import { getDataOrThrow } from "../../services/api.js";
 import { getUserDashboardFriends } from "../../services/userStatsServices.js";
 import { toaster } from "../../Toaster.js";
@@ -101,7 +101,7 @@ export class FriendsTab extends AbstractTab {
 
   renderFriendSelector(friends: FriendStatsSeries) {
     if (!friends || friends.length === 0) {
-      appLogger.warn("No friends to display");
+      viewLogger.warn("No friends to display");
       return;
     }
     const container = getById<HTMLDivElement>("friend-selector");
